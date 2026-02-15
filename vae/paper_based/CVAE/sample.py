2a717eed848281b25ac4846456247e64aa6b0186
johanbaumann <johanbau@student.chalmers.se>
2026-02-15 22:57:31 +0100

    With result from first run och CVAE, will rewrite code, and change so it only needs LOGP!

--- vae/paper_based/CVAE/sample.py
+++ vae/paper_based/CVAE/sample.py
@@ -9,7 +9,7 @@ from rdkit.Chem.rdMolDescriptors import CalcTPSA
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size', help='batch_size', type=int, default=128)
-parser.add_argument('--num_iteration', help='num_iteration', default=10)
+parser.add_argument('--num_iteration', help='num_iteration', type=int, default=10)
 parser.add_argument('--latent_size', help='latent_size', type=int, default=200)
 parser.add_argument('--unit_size', help='unit_size of rnn cell', type=int, default=512)
 parser.add_argument('--n_rnn_layer', help='number of rnn layer', type=int, default=3)
@@ -24,27 +24,52 @@ parser.add_argument('--result_filename', help='name of result filename', type=st
 parser.add_argument('--lr', help='learning rate', type=float, default=0.0001)
 args = parser.parse_args()
 
+
+config = {
+    'batch_size': 128,
+    'num_iteration': 10, # number of iteration to generate smiles
+    'latent_size': 200,
+    'unit_size': 512,
+    'n_rnn_layer': 3,
+    'seq_length': 120,
+    'mean': 0.0,
+    'stddev': 1.0,
+    'num_prop': 3,
+    'save_file': 'save/model_.ckpt-99.pt',
+    'target_prop': '300.0 3.0 75.0', # target properties (MW, LogP, TPSA) to generate smiles. You can change this value to generate different smiles.
+    'prop_file': "smiles_prop.txt",
+    'result_filename': 'result.txt',
+    'lr': 0.0001
+}
+
+for key in config:
+    value = getattr(args, key, None)
+    if value is not None:
+        config[key] = value
+
+
+
 #convert smiles to numpy array
-_, _, char, vocab, _, _ = load_data(args.prop_file, args.seq_length)
+_, _, char, vocab, _, _ = load_data(config['prop_file'], config['seq_length'])
 vocab_size = len(char)
 
 #model and restore model parapmeters
 model = CVAE(vocab_size,
-             args
+             config,
              )
-model.restore(args.save_file)
+model.restore(config['save_file'])
 
 print('Number of parameters : ', sum(p.numel() for p in model.parameters() if p.requires_grad))
 
 #target property to numpy array
-target_prop = np.array([[float(p) for p in args.target_prop.split()] for _ in range(args.batch_size)])
-start_codon = np.array([np.array(list(map(vocab.get, 'X')))for _ in range(args.batch_size)])
+target_prop = np.array([[float(p) for p in config['target_prop'].split()] for _ in range(config['batch_size'])])
+start_codon = np.array([np.array(list(map(vocab.get, 'X')))for _ in range(config['batch_size'])])
 
 #generate smiles
 smiles = []
-for _ in range(args.num_iteration):
-    latent_vector = np.random.normal(args.mean, args.stddev, (args.batch_size, args.latent_size))
-    generated = model.sample(latent_vector, target_prop, start_codon, args.seq_length)
+for _ in range(config['num_iteration']):
+    latent_vector = np.random.normal(config['mean'], config['stddev'], (config['batch_size'], config['latent_size']))
+    generated = model.sample(latent_vector, target_prop, start_codon, config['seq_length'])
     smiles += [convert_to_smiles(generated[i], char) for i in range(len(generated))]
 
 #write smiles and calcualte properties of them    
@@ -54,7 +79,7 @@ print ('number of generate smiles (after remove duplicated ones) : ', len(smiles
 ms = [Chem.MolFromSmiles(s) for s in smiles]
 ms = [m for m in ms if m is not None]
 print ('number of valid smiles : ', len(ms))
-with open(args.result_filename, 'w') as w:
+with open(config['result_filename'], 'w') as w:
     w.write('smiles\tMW\tLogP\tTPSA\n')
     for m in ms:
         try:
