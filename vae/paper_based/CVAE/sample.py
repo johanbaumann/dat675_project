ab6012fd0e3a5c10e6781b09f73a4dbd9a469c31
Johan Baumann <112492294+johanbaumann@users.noreply.github.com>
2026-02-16 08:07:50 +0100

    Added some tests running on my laptop to see how much time it takes to generate

--- vae/paper_based/CVAE/sample.py
+++ vae/paper_based/CVAE/sample.py
@@ -2,6 +2,8 @@ from model import CVAE
 from utils import *
 import numpy as np
 import argparse
+import os
+import time as t
 from rdkit import Chem
 from rdkit.Chem.Descriptors import ExactMolWt
 from rdkit.Chem.Crippen import MolLogP
@@ -24,7 +26,7 @@ parser.add_argument('--result_filename', help='name of result filename', type=st
 parser.add_argument('--lr', help='learning rate', type=float, default=0.0001)
 args = parser.parse_args()
 
-
+start = t.time()
 config = {
     'batch_size': 128,
     'num_iteration': 10, # number of iteration to generate smiles
@@ -38,7 +40,7 @@ config = {
     'save_file': 'save/model_.ckpt-99.pt',
     'target_prop': '300.0 3.0 75.0', # target properties (MW, LogP, TPSA) to generate smiles. You can change this value to generate different smiles.
     'prop_file': "smiles_prop.txt",
-    'result_filename': 'result.txt',
+    'result_filename': 'test_laptop_res.txt',
     'lr': 0.0001
 }
 
@@ -113,7 +115,8 @@ print('average LogP : ', avg_logp(ms))
 # save smiles and properties to file
 df.to_csv(config['result_filename'], index=False)
 
-
+end_time = t.time()
+print(f'time to run: {end_time - start}')
 
 
 #
